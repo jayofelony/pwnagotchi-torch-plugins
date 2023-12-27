0880f48092c957341192e6d118e38a7eb457eac5
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2023-12-27 23:07:34 +0100

    Update show_pwd.py

--- show_pwd.py
+++ show_pwd.py
@@ -9,7 +9,6 @@ import os
 class ShowPwd(plugins.Plugin):
     __author__ = '@nagy_craig'
     __version__ = '1.0.0'
-    __name__ = "Show Pwd"
     __license__ = 'GPL3'
     __description__ = 'A plugin to display recently cracked passwords'
 
